e08bbd26190ef61d14bfebcbf8fdb9ba0a5202ea
Mohamedamine2005 <47900954+Mohamedamine2005@users.noreply.github.com>
2019-03-15 17:57:01 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,5 +36,6 @@ client.on('ready', () => {
  
 client.on('ready',async () => {
   client.channels.find(ch => ch.id === "551419457336115213" && ch.type === 'voice').join();
-});
+})
+;
 client.login(process.env.BOT_TOKEN);
